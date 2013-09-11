a50f208427d6f08a375844df104de57c06e10ae2
tonwell <wellington.pbi10@gmail.com>
2013-09-10 23:23:37 -0300

    hope it works

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Androidlista::Application.config.secret_key_base = '2479fb9e17319dc88c02c56920709b92f467d91dcf5c198f4c4629579bb6eb2625eab05fe40ec9050c5bcc48aff705c2604b4b5a0ee167127a67cbdb82086e81'
